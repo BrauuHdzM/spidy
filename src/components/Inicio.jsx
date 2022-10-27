5b58ce4671315d6db722497a6ba14949d7065c9b
Daniela Montserrat Enriquez Ballesteros <82247636+HatshepsutS@users.noreply.github.com>
2022-10-27 12:21:29 -0500

    F carrusel
    por fin se murió el bastardo

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,63 +1,27 @@
-import React, { useEffect, useState } from 'react'
+
 import Carousel from 'react-bootstrap/Carousel';
-import imagenprueba from '../images/imagenprueba.jpg';
 import portada1 from '../images/portada1.jpg';
 import portada2 from '../images/portada2.jpg';
 import portada3 from '../images/portada3.jpg';
-import prueba2 from '../images/prueba2.jpg';
 import Button from 'react-bootstrap/Button';
-import { BsTypeH2 } from 'react-icons/bs';
 
-
+import SpydyLogo from '../images/SpydyLogo.svg';
 
 export const Inicio = () => {
 
   return (
     
     <>
+   <div class="container">
+   <div class="row mt-4">
+   <div class="col-md-4"> 
+   </div>
+   <img src={SpydyLogo} class="img-responsive" width="307" height="240"  ></img>
    
+   </div>
+   </div>
    
-    <Carousel variant='light'>
-      <Carousel.Item>
-      <a href="/Escanear">
-        <img
-          className="d-block w-100"
-          src={portada1}
-          alt="First slide" />
-        <Carousel.Caption>
-          <h3>¿Te preocupó la araña que viste?</h3>
-          <p>Escanea tu preocupación aquí</p>
-        </Carousel.Caption>
-       </a> 
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <a href="/InformacionArana">
-        <img
-          className="d-block w-100"
-          src={portada2}
-          alt="Second slide" />
-        <Carousel.Caption>
-          <h3>¿Cuáles son especies de arañas peligrosas?</h3>
-          <p>Descubrelo aquí</p>
-        </Carousel.Caption>
-        </a>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <a href="/Nosotros">
-        <img
-          className="d-block w-100"
-          src={portada3}
-          alt="Third slide" />
-        <Carousel.Caption>
-          <h3>¿Tienes alguna duda o sugerencia?</h3>
-          <p>Contáctanos aquí</p>
-        </Carousel.Caption>
-        </a>
-      </Carousel.Item>
-
-    </Carousel><br></br><br></br>
+ 
    
     <div style={{ height: '30vh' }}>
       
@@ -71,18 +35,9 @@ export const Inicio = () => {
         </Button>
         </div>
 
-      </div><br></br><div>
-        <br></br>
-        <div class>
-        <h5 class="text-center"><font color="white">Acerca de este proyecto</font></h5>
-        </div>
-        <p class="text-justify"><font color="white">Pues este proyecto nace de que el compa de Ingeniería de software dijo sorprendanme y pues
-          pues aquí está, nacido de la creatividad de spiderman o algo asi, pero es trabajo honesto jeje.
-          Espero que les guste y que les sirva de algo, si no pues no se que decirles, pero bueno, ahora toca aprende
-          a usar Tensorflow y a hacer un modelo de machine learning para que reconozca las arañas, pero eso es para otro día.
-          </font>
-        </p>
-      </div>
+      </div><br></br>
+      
+   
     
       </>
     
