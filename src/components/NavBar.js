f38b65e47955b189258df878d5ffbb8d481c0f05
HatshepsutS <denriquezmont@gmail.com>
2022-12-31 13:51:15 -0600

    Diseño del Login terminado
    Se termino el diseñol del Login .

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import SpidyBar from '../images/SpidyBar.svg';
+import SpidyAdmin from '../images/SpidyAdmin.png';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
@@ -33,15 +34,22 @@ function NavBar() {
               </Nav>
               <Nav>
             <Button variant="outline-light" onClick={handleShow}>
-        Login administrador
+        SpidyAdmin
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} center styles={{ overlay: { background: "#e13743" } }} >
+      <div class="logindiv">
         <Modal.Header closeButton>
-          <Modal.Title>Login Administrador</Modal.Title>
+      
+        <div class="mt-2 mb-2 ms-3 me-2 ">
+    
+          <Modal.Title> <center><img src={SpidyAdmin} class="img-fluid" ></img></center> </Modal.Title>
+          </div>
         </Modal.Header>
+        </div>
         <Modal.Body>
         <Form>
+        <div class="mt-1 mb-2.5 ms-4 me-4 ">
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Correo electrónico</Form.Label>
               <Form.Control
@@ -55,16 +63,21 @@ function NavBar() {
         <Form.Label>Contraseña </Form.Label>
         <Form.Control type="password" placeholder="Contraseña" />
       </Form.Group>
+      <div class="text-center">
+          <Button variant="danger" id="escaner" onClick={handleClose}>
+            Iniciar sesión 
+          </Button>
+          </div>
+      </div>
           </Form>
           
           
           
           </Modal.Body>
         <Modal.Footer>
-      
-          <Button variant="primary" onClick={handleClose}>
-            Iniciar sesión
-          </Button>
+        <div class="text-center">
+        <i><p>Este es un apartado exclusivo para administradores de Spidy, dudas o comentarios spidyadmin@spidy.com</p></i>
+        </div>
         </Modal.Footer>
       </Modal>  
          
