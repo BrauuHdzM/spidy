bf4834c5a5aae98f08dcdafd3f9f4e3a1ac9e6d7
HatshepsutS <denriquezmont@gmail.com>
2023-01-01 21:49:57 -0600

    SpidyAdmin added

--- src/components/AdminBar.js
+++ src/components/AdminBar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react' ;
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import SpidyBar from '../images/SpidyBar.svg';
+import spidyAdminBar from '../images/spidyAdminBar.svg';
 import SpidyAdmin from '../images/SpidyAdmin.png';
 import Button from 'react-bootstrap/Button';
 
@@ -14,9 +14,9 @@ function AdminBar(props) {
   const handleShow = () => setShow(true);
 
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" >
       <Container>
-        <Navbar.Brand ><img src={SpidyBar}></img><font color="#222529">Spiiy</font></Navbar.Brand>
+        <Navbar.Brand ><img src={spidyAdminBar}></img><font color="#222529">spidybaradmiinaaaaaaaaaa</font></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
