2f755612676327d1629ac6324f24869789037806
HatshepsutS <denriquezmont@gmail.com>
2023-01-05 13:58:29 -0600

    Styling

--- src/components/ReporteGeneral.js
+++ src/components/ReporteGeneral.js
@@ -1,10 +1,64 @@
-import { PDFDownloadLink, Document, Page,Text } from '@react-pdf/renderer';
+import { PDFDownloadLink, Document, Page,Text,Image } from '@react-pdf/renderer';
+
+import portada1 from '../images/portada1.jpg';
 import React from "react";
+
+
 export const ReporteG = () => (
     <Document>
       <Page>
-        <Text>Probando Dependencia</Text>
+      <Image src={portada1} />
+    
       </Page>
+
+
+
     </Document>
   );
-  
+ /* const styles = StyleSheet.create({
+    body: {
+      paddingTop: 35,
+      paddingBottom: 65,
+      paddingHorizontal: 35,
+    },
+    title: {
+      fontSize: 24,
+      textAlign: 'center',
+      fontFamily: 'Oswald'
+    },
+    author: {
+      fontSize: 12,
+      textAlign: 'center',
+      marginBottom: 40,
+    },
+    subtitle: {
+      fontSize: 18,
+      margin: 12,
+      fontFamily: 'Oswald'
+    },
+    text: {
+      margin: 12,
+      fontSize: 14,
+      textAlign: 'justify',
+      fontFamily: 'Times-Roman'
+    },
+    image: {
+      marginVertical: 15,
+      marginHorizontal: 100,
+    },
+    header: {
+      fontSize: 12,
+      marginBottom: 20,
+      textAlign: 'center',
+      color: 'grey',
+    },
+    pageNumber: {
+      position: 'absolute',
+      fontSize: 12,
+      bottom: 30,
+      left: 0,
+      right: 0,
+      textAlign: 'center',
+      color: 'grey',
+    },
+  });*/
