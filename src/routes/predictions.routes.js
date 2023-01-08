47b25444cda6273d9417499f8d01b4e218194957
Kronos <mauricio_noble6@hotmail.com>
2023-01-08 00:47:48 -0600

    Insercion de prediccion y respuesta con id de la base de datos

--- src/routes/predictions.routes.js
+++ src/routes/predictions.routes.js
@@ -19,67 +19,28 @@ const fileUpload = multer({
 
 //Funcion guardar imagen en la base de datos
 router.post('/savePrediction', fileUpload, (req, res) => {
+    const prediction = req.body.prediction;
+    const date = new Date(Date.now()).toISOString().slice(0, 19).replace('T', ' ');
+    const storagePath = req.file.filename;
     console.log(req.body.prediction)
     console.log(req.file)
-        //const data = fs.readFileSync(path.join(__dirname, '../images/' + req.file.filename))
+    console.log(req.file.filename);
 
-
-})
-
-//Ejemplo de query a la base de datos
-router.get('/consulta', (req, res) => {
     req.getConnection((err, conn) => {
         if (err) return res.status(500).send('server error')
 
-
-        //Insert 
-        //Ejemplo con parametros
-        /*conn.query('INSERT INTO image set ?', [{type, name, data}], (err, rows) => {
-            if(err) return res.status(500).send('server error')
-
-            res.send('image saved!')
-        })*/
-        //Ejemplo todo en un una cadena
-        conn.query(`INSERT INTO spiders(species, medicallySignificant, description, imgPath) VALUES('MAURICIO', 1, 'ES FEO', 'PATH'); 
-        SELECT LAST_INSERT_ID() AS ID;`, (err, rows, filds) => {
+        conn.query('INSERT INTO predictions set ?;SELECT LAST_INSERT_ID() AS ID;', [{ prediction, date, storagePath }], (err, rows, filds) => {
             if (err) {
                 console.error('Error al ejecutar la consulta:', err);
                 return;
             }
             let id = Object.values(JSON.parse(JSON.stringify(rows)));
-            //const id = rows[1].constructor.ID;
             console.log(id[1][0].ID);
-            res.send(id)
+            res.send(id[1][0]);
         })
+    });
 
-        //Consulta 
-        /*connection.query('SELECT * FROM tabla', function (error, results, fields) {
-            if (error) throw error;
-            console.log('Los resultados de la consulta son: ', results);
-          });*/
-    })
-})
-
-/*
-router.post('/images/post', fileUpload,(req, res) => {
-
-req.getConnection((err, conn) => {
-    if(err) return res.status(500).send('server error')
-
-        const type = req.file.mimetype
-        const name = req.file.originalname
-        const data = fs.readFileSync(path.join(__dirname, '../images/' + req.file.filename))
-
-        conn.query('INSERT INTO image set ?', [{type, name, data}], (err, rows) => {
-            if(err) return res.status(500).send('server error')
-
-            res.send('image saved!')
-        })
-    })
-    
-})
-*/
-
+});
 
 
 
