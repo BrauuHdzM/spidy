f897876bb08fff41ec59849a1eec460e9a149000
Kronos <mauricio_noble6@hotmail.com>
2023-01-08 01:56:55 -0600

    Descarga de imagenes terminado

--- src/components/AdminPanel.jsx
+++ src/components/AdminPanel.jsx
@@ -1,11 +1,11 @@
 
-import React,{useRef,useState,useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Bar } from "react-chartjs-2";
 import { UserData } from "./Data";
-import {ReporteG } from "./ReporteGeneral";
+import { ReporteG } from "./ReporteGeneral";
 import Container from 'react-bootstrap/Container';
 import pataslargas from '../images/pataslargas.jpg';
-import { PDFDownloadLink, PDFViewer,Document, Page } from '@react-pdf/renderer';
+import { PDFDownloadLink, PDFViewer, Document, Page } from '@react-pdf/renderer';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import PieChart from "./PieChart";
@@ -15,74 +15,71 @@ import { Button } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
 import { Pie } from "react-chartjs-2";
 export const AdminPanel = () => {
-  
-  const barra=useRef(null);
-  const pastel=useRef(null);
-  const [spdmost,setspdmost]=useState('Eremobates');
-  const [prc,setprc]=useState('88');
-  const [img, setimg] =useState({pataslargas});
-  const [img2, setimg2] =useState({pataslargas});
+
+  const barra = useRef(null);
+  const pastel = useRef(null);
+  const [spdmost, setspdmost] = useState('Eremobates');
+  const [prc, setprc] = useState('88');
+  const [img, setimg] = useState({ pataslargas });
+  const [img2, setimg2] = useState({ pataslargas });
   const [result, setResult] = useState([]);
-  
+
   useEffect(() => {
     axios.post("/admin/adminData")
-    .then((response)=>{
-      setResult(JSON.parse(JSON.stringify(response.data)));
-      
-    })
+      .then((response) => {
+        setResult(JSON.parse(JSON.stringify(response.data)));
 
-    
+      })
+
+
   }, [])
 
 
   const handleClick = () => {
     console.log("zapata");
     console.log(spdmost);
-  const chart = barra.current;
-  setimg (chart.toBase64Image('image/png', 1));
-  const chart2 = pastel.current;
-  setimg2 (chart2.toBase64Image('image/png', 1));
+    const chart = barra.current;
+    setimg(chart.toBase64Image('image/png', 1));
+    const chart2 = pastel.current;
+    setimg2(chart2.toBase64Image('image/png', 1));
 
 
-  console.log(img)
-  console.log(img2)
+    console.log(img)
+    console.log(img2)
 
-  setShow(true);
-  } 
-  
-  
+    setShow(true);
+  }
+
+
   const [show, setShow] = useState(false);
 
 
-  const  getData = () => {
-   setUserData({
+  const getData = () => {
+    setUserData({
       labels: result.map((row) => row.species),
       datasets: [
         {
           label: "Cantidad de registros",
-          data: result.map((row) =>row.CantidadEncuestas),
-          backgroundColor: ["#cd0c36", "#000000","#ee4242","#fd7b7b", "#565656","#989898","#ffffff","#670f22", ],
+          data: result.map((row) => row.CantidadEncuestas),
+          backgroundColor: ["#cd0c36", "#000000", "#ee4242", "#fd7b7b", "#565656", "#989898", "#ffffff", "#670f22",],
           borderColor: "black",
           borderWidth: 1,
         },
       ],
     });
 
-  /*
-
-    axios.post("/predictions/adminData").then((response)=>{
-      const diosayuda=JSON.stringify(response.data); 
-      console.log(diosayuda);
-     
-
-
-
-      });
-      */
-    }
+    /*
   
-
-
+      axios.post("/predictions/adminData").then((response)=>{
+        const diosayuda=JSON.stringify(response.data); 
+        console.log(diosayuda);
+       
+  
+  
+  
+        });
+        */
+  }
 
 
   const handleClose = () => {
@@ -90,118 +87,114 @@ export const AdminPanel = () => {
   };
   const [userData, setUserData] = useState({
     labels: UserData.map((row) => row.species),
-      datasets: [
-        {
-          label: "Cantidad de registros",
-          data: UserData.map((row) =>row.CantidadEncuestas),
-          backgroundColor: ["#cd0c36", "#000000","#ee4242","#fd7b7b", "#565656","#989898","#ffffff","#670f22", ],
-          borderColor: "black",
-          borderWidth: 1,
-        },
+    datasets: [
+      {
+        label: "Cantidad de registros",
+        data: UserData.map((row) => row.CantidadEncuestas),
+        backgroundColor: ["#cd0c36", "#000000", "#ee4242", "#fd7b7b", "#565656", "#989898", "#ffffff", "#670f22",],
+        borderColor: "black",
+        borderWidth: 1,
+      },
     ],
   });
 
 
-  
-
-
   return (
-    
+
     <>
 
-<Container>
-      <Row>
-        <Col>
-       
-        <div  className='admindiv'>
-     
-       <h1 class="text-center">Registros totales en el sistema</h1>
-       <Button onClick={getData}>Presione para inciar graficación</Button>
-       <Bar data={userData} ref={barra} />;
-      
-      </div>
-      </Col>
-     
-      </Row>
-      <br></br><br></br>
-      <Row>
-      
+      <Container>
+        <Row>
+          <Col>
 
-      <Col>
-      <div  className='admindiv'>
-      <h1 class="text-center">Satisfacción general del sistema</h1>
-      <div className='piechartdiv' >
-      <p class="text-center"> 
-      <Pie data={userData} ref={pastel}  /></p>
-      </div>
-      </div>
-      </Col>
+            <div className='admindiv'>
 
-      <Col>
-      <div  className='admindiv'>
-      <h1 class="text-center" >Satisfacción por araña</h1>
-      <div className='piechartdiv'>
-     <p class="text-center"> <PieChart chartData={userData} /></p>
-      </div>
-      </div>
-      </Col>
-      
-      </Row>
-      <br></br><br></br>
-      <Row>
-     
-<Col>
-<div  className='admindiv'>
-<h1  class="text-center">Descargas</h1>
-<Button className="btn btn-dark text-center">
-<p class="text-center"><i><FaFileDownload size="2em" /><a>Haga click aquí para descargar el dataset completo</a></i></p>
-</Button>
-</div>
-</Col>
-<Col>
-<div  className='admindiv'>
-<h1 class="text-center">Generación de reportes</h1>
- 
-<div>
+              <h1 class="text-center">Registros totales en el sistema</h1>
+              <Button onClick={getData}>Presione para inciar graficación</Button>
+              <Bar data={userData} ref={barra} />;
 
-<Modal show={show} onHide={handleClose}    
-      aria-labelledby="contained-modal-title-vcenter" size="lg"
-      centered >
-        <Modal.Header closeButton>
-    
-          <Modal.Title>  <h1 class='text-center'> Reporte general de la aplicación</h1> </Modal.Title>
-        
-        </Modal.Header>
-      
-        <Modal.Body>
-        <PDFDownloadLink document={<ReporteG imgbarras={img} spidermost={spdmost} imgpastel={img2} porcentaje={prc}/>} fileName="ReporteGeneral.pdf">
-    <p className='text-center'>Descargar reporte general</p>
-    </PDFDownloadLink>
-    <p className='text-center'>
-       <PDFViewer height="500em" width="600em">
-       <ReporteG imgbarras={img} spidermost={spdmost} imgpastel={img2} porcentaje={prc}/>
-          </PDFViewer>
-          </p>
-          </Modal.Body>
-          </Modal>
+            </div>
+          </Col>
+
+        </Row>
+        <br></br><br></br>
+        <Row>
+
+
+          <Col>
+            <div className='admindiv'>
+              <h1 class="text-center">Satisfacción general del sistema</h1>
+              <div className='piechartdiv' >
+                <p class="text-center">
+                  <Pie data={userData} ref={pastel} /></p>
+              </div>
+            </div>
+          </Col>
+
+          <Col>
+            <div className='admindiv'>
+              <h1 class="text-center" >Satisfacción por araña</h1>
+              <div className='piechartdiv'>
+                <p class="text-center"> <PieChart chartData={userData} /></p>
+              </div>
+            </div>
+          </Col>
+
+        </Row>
+        <br></br><br></br>
+        <Row>
+
+          <Col>
+            <div className='admindiv'>
+              <h1 class="text-center">Descargas</h1>
+
+              <Button className="btn btn-dark text-center" href='/admin/descargarImagenes'>
+                <p class="text-center"><i><FaFileDownload size="2em" /><a>Haga click aquí para descargar el dataset completo</a></i></p>
+              </Button>
 
 
 
+            </div>
+          </Col>
+          <Col>
+            <div className='admindiv'>
+              <h1 class="text-center">Generación de reportes</h1>
 
-  <Button onClick={handleClick}> Ver reporte general de la aplicación </Button>
-  
-    
-   
-  </div>
-<p><a>Descargar reporte personalizado</a></p>
-</div>   
-</Col>
+              <div>
 
-      </Row>
+                <Modal show={show} onHide={handleClose}
+                  aria-labelledby="contained-modal-title-vcenter" size="lg"
+                  centered >
+                  <Modal.Header closeButton>
 
-    </Container>
-    <br></br>  <br></br>
-      </>
-    
+                    <Modal.Title>  <h1 class='text-center'> Reporte general de la aplicación</h1> </Modal.Title>
+
+                  </Modal.Header>
+
+                  <Modal.Body>
+                    <PDFDownloadLink document={<ReporteG imgbarras={img} spidermost={spdmost} imgpastel={img2} porcentaje={prc} />} fileName="ReporteGeneral.pdf">
+                      <p className='text-center'>Descargar reporte general</p>
+                    </PDFDownloadLink>
+                    <p className='text-center'>
+                      <PDFViewer height="500em" width="600em">
+                        <ReporteG imgbarras={img} spidermost={spdmost} imgpastel={img2} porcentaje={prc} />
+                      </PDFViewer>
+                    </p>
+                  </Modal.Body>
+                </Modal>
+
+                <Button onClick={handleClick}> Ver reporte general de la aplicación </Button>
+
+              </div>
+              <p><a>Descargar reporte personalizado</a></p>
+            </div>
+          </Col>
+
+        </Row>
+
+      </Container>
+      <br></br>  <br></br>
+    </>
+
   )
 }
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -10,7 +10,7 @@ router.post('/login', (req, res) => {
 
     req.getConnection((err, conn) => {
         if (err) return res.status(500).send('server error')
-        conn.query("select * from administrators where username = ?  and password = ?", [usuario, password], function(error, results, fields) {
+        conn.query("select * from administrators where username = ?  and password = ?", [usuario, password], function (error, results, fields) {
             if (error) throw error;
             if (results) {
                 res.send(results);
@@ -25,7 +25,7 @@ router.post('/adminData', (req, res) => {
 
     req.getConnection((err, conn) => {
         if (err) return res.status(500).send('server error')
-        conn.query("SELECT spiders.idSpider as id, spiders.species, COUNT(*) as 'CantidadEncuestas' FROM spiders INNER JOIN predictions ON spiders.idSpider = predictions.prediction INNER JOIN surveys ON predictions.idPrediction= surveys.idPrediction group by spiders.idSpider", function(error, results, fields) {
+        conn.query("SELECT spiders.idSpider as id, spiders.species, COUNT(*) as 'CantidadEncuestas' FROM spiders INNER JOIN predictions ON spiders.idSpider = predictions.prediction INNER JOIN surveys ON predictions.idPrediction= surveys.idPrediction group by spiders.idSpider", function (error, results, fields) {
             if (error) res.send(error);
             if (results) {
                 res.send(results);
@@ -41,7 +41,7 @@ router.get('/descargarImagenes', (req, res) => {
     const zip = new AdmZip();
 
     // Lee todos los archivos de la carpeta "imagenes"
-    fs.readdir('imagesUploaded', (error, archivos) => {
+    fs.readdir('./src/imagesUploaded', (error, archivos) => {
         if (error) {
             res.status(500).send('Error al leer la carpeta de imágenes');
             return;
@@ -49,7 +49,7 @@ router.get('/descargarImagenes', (req, res) => {
 
         // Agrega todos los archivos a la instancia de AdmZip
         archivos.forEach(archivo => {
-            const contenido = fs.readFileSync(`imagenes/${archivo}`);
+            const contenido = fs.readFileSync(`./src/imagesUploaded/${archivo}`);
             zip.addFile(archivo, contenido, '', 0o644 << 16);
         });
 
