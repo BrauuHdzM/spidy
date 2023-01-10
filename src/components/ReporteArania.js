eac884da9a7f24fdba99f05e9d0b87c79bf5f09a
HatshepsutS <denriquezmont@gmail.com>
2023-01-09 21:02:03 -0600

    Avance de Reporte personalizado
    Se creo el componente y los banners

---
+++ src/components/ReporteArania.js
@@ -0,0 +1,73 @@
+import {  Document, Page,Text,Image,StyleSheet,Font,View   } from '@react-pdf/renderer';
+import React from "react";
+import portada3 from '../images/portada3.jpg';
+import TarantulaPic from '../images/rP/r2Tar.jpg';
+
+
+
+
+const current = new Date();
+  const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+
+export const ReporteSpider = ({img}) => (
+    <Document>
+      <Page>
+      <Image src={portada3} />
+      <Image src={img} />
+
+      
+      <Text style={styles.pageNumber}>Copyright © 2023 All Rights Reserved by Spidyyy</Text> 
+      </Page>
+
+
+
+    </Document>
+  );
+
+ /* Font.register({ family: 'Staatliches-Regular', format: "truetype", src:font });*/
+ 
+ Font.register({
+  family: 'Oswald',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+});
+  const styles = StyleSheet.create({
+  
+    body: {
+      paddingTop: 10,
+      paddingHorizontal: 35,
+    },
+    body2: {
+      paddingTop: 3,
+      paddingHorizontal: 170,
+    },
+    bodyb: {
+      paddingTop: 1,
+      paddingHorizontal: 35,
+    },
+    title: {
+      fontSize: 20,
+      textAlign: 'center',
+      fontFamily: 'Oswald'
+    },
+    textran: {
+      fontSize: 12,
+      textAlign: 'center',
+      fontFamily: 'Oswald',
+      color: 'red',
+    },
+    image: {
+      marginVertical: 15,
+      marginHorizontal: 100,
+      width: 12,
+      height: 20
+    },
+    pageNumber: {
+      position: 'absolute',
+      fontSize: 12,
+      bottom: 30,
+      left: 0,
+      right: 0,
+      textAlign: 'center',
+      color: 'grey',
+    },
+  });
