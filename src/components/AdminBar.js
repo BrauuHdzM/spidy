7b82032eb625f17add9f1056e4c1eac1ea2cb513
Kronos <mauricio_noble6@hotmail.com>
2023-01-12 20:40:24 -0600

    Navbar Admin responsivo

--- src/components/AdminBar.js
+++ src/components/AdminBar.js
@@ -1,37 +1,38 @@
-import React, { useState, useEffect } from 'react' ;
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import spidyAdminBar from '../images/spidyAdminBar.svg';
 import SpidyAdmin from '../images/SpidyAdmin.png';
 import Button from 'react-bootstrap/Button';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function AdminBar(props) {
- 
- 
+
+
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const red= () => {
-   
-    props.logout() 
+  const red = () => {
+
+    props.logout()
   };
 
   return (
     <Navbar bg="dark" variant="dark" >
       <Container>
-        <Navbar.Brand ><img src={spidyAdminBar}></img><font color="#222529">spidybaradmiinaaaaaaaaaa</font></Navbar.Brand>
+        <Navbar.Brand ><img src={spidyAdminBar} height="40"
+          className="d-inline-block align-top"></img><font color="#222529">spidy</font></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">  
-              </Nav>
-              <Nav>
-              <Link to="/">  <Button variant="outline-light" onClick={red}>
-        Cerrar sesión
-      </Button></Link>
+          <Nav className="me-auto">
+          </Nav>
+          <Nav>
+            <Link to="/">  <Button variant="outline-light" onClick={red}>
+              Cerrar sesión
+            </Button></Link>
 
           </Nav>
         </Navbar.Collapse>
@@ -40,4 +41,4 @@ function AdminBar(props) {
   );
 }
 
-export {AdminBar} ;
+export { AdminBar };
