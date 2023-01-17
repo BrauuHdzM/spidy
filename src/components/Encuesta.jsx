221ecaefa807ff828707b6ff275a9debbf41b4a1
HatshepsutS <denriquezmont@gmail.com>
2023-01-16 20:32:21 -0600

    Botones encuesta
    La verdad no me gustaron pero ya alv

--- src/components/Encuesta.jsx
+++ src/components/Encuesta.jsx
@@ -3,7 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFaceFrown, faFaceMeh, faFaceSmile } from '@fortawesome/free-solid-svg-icons';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export const Encuesta = (props) => {
 
     const [show, setShow] = useState(true);
@@ -51,12 +52,25 @@ export const Encuesta = (props) => {
         <div>
             {show ?
                 (<div>
-                    <Button onClick={handleClick1}><FontAwesomeIcon icon={faFaceFrown} /></Button>
-                    <Button onClick={handleClick2}><FontAwesomeIcon icon={faFaceMeh} /></Button>
-                    <Button onClick={handleClick3}><FontAwesomeIcon icon={faFaceSmile} /></Button>
+                    <p class="text-center"><i> <font color="grey"> ¡Ayúdanos a seguir mejorando Spidy!. Califica tu experiencia</font></i> </p>
+                   <p class="text-center">
+                    <Row>
+                        <Col>
+                        <div class="mx-auto">
+                    <Button onClick={handleClick1} className="btnEncuesta btn btn-danger" ><p><br></br> <FontAwesomeIcon icon={faFaceFrown} size="lg"/><br></br>Pésima</p></Button>
+                    </div>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                    <Button onClick={handleClick2} className="btnEncuesta btn btn-danger"><p><br></br><FontAwesomeIcon icon={faFaceMeh}  size="lg"/><br></br>Regular</p></Button>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                    <Button onClick={handleClick3} className="btnEncuesta btn btn-danger "><p><br></br><FontAwesomeIcon icon={faFaceSmile} size="lg"/><br></br>Positiva</p></Button>
+                    </Col>
+                    </Row>
+                    </p>
                 </div>)
                 :
-                (<p>Encuesta contestada</p>)
+                (<p class="text-center"><i> <font color="grey">Muchas gracias, tus comentarios nos ayudan a seguir mejorando Spidy</font></i></p>)
             }
         </div>
     );
